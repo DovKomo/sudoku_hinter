8c8262c5de16baa6419f1b4006a8fec75740eb26
Dovile <dovile.komolovaite@ktu.edu>
2021-09-24 17:00:42 +0300

    isolate grid coordinates

--- main.py
+++ main.py
@@ -41,24 +41,60 @@ print(processed_image.shape)
 # 2. Sudoku Extraction
 # -----------------------
 
-def get_full_grid(img):
+def get_grid_coordinates(img, show=False):
     grid_img = 255 * np.ones((img.shape[0], img.shape[1], 3), np.uint8)  # white frame
-    contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    contours, _ = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    print(f'len contours: {len(contours)}')
+    grid_contours = []
 
     for contour in contours:
         area = cv2.contourArea(contour)
         if area > 100:
             cv2.drawContours(grid_img, contour, -1, (0, 0, 0), 1)
 
-    cv2.imshow('Contours', grid_img)
+            # =------------------------
+            coordinates = contour.ravel()
+            # print('coordinates: ', len(coordinates))
+            x_coord = coordinates[::2]
+            # print('x_coord: ', len(x_coord))
+            y_coord = coordinates[1:][::2]
+            # print('y_coord: ', len(y_coord))
+
+            x_min = x_coord.min()
+            x_max = x_coord.max()
+
+            y_min = y_coord.min()
+            y_max = y_coord.max()
+
+            # =------------------------
+
+            grid_contours.append([x_min, x_max, y_min, y_max])
+    if show:
+        cv2.imshow('Contours', grid_img)
+        cv2.waitKey(0)
+    return grid_contours
+
+
+grid_contours = get_grid_coordinates(processed_image, show=False)
+print('grid_contours: ', grid_contours)
+print('end')
+
+
+# draw four corners:
+def plot_dots_on_grid_corners(img, dot_size=5):
+    grid_cntrs = get_grid_coordinates(img, show=False)
+    for i in range(len(grid_cntrs)):
+        cv2.circle(img, (grid_cntrs[i][0], grid_cntrs[i][2]), dot_size, [0, 0, 255], -1)
+        cv2.circle(img, (grid_cntrs[i][1], grid_cntrs[i][2]), dot_size, [0, 0, 255], -1)
+        cv2.circle(img, (grid_cntrs[i][0], grid_cntrs[i][3]), dot_size, [0, 0, 255], -1)
+        cv2.circle(img, (grid_cntrs[i][1], grid_cntrs[i][3]), dot_size, [0, 0, 255], -1)
+    cv2.imshow("dots on grid corners", processed_image)
     cv2.waitKey(0)
-    return grid_img
+    cv2.destroyAllWindows()
 
 
-grid_img = get_full_grid(processed_image)
+plot_dots_on_grid_corners(processed_image)
 
-
-# todo: calculate the sizes of the grid: coordinates of every box
 # todo: reuse coordinates for indicating wheather box is empty or with number
 
 # def get_grid_lines(img, horizontal_size = 11, vertical_size = 11):
@@ -123,3 +159,7 @@ grid_boxes = get_grid_boxes(processed_image, show=True)
 
 # hand written digits:
 #  https://scikit-learn.org/stable/auto_examples/classification/plot_digits_classification.html
+
+# more:
+# https://towardsdatascience.com/solve-sudokus-automatically-4032b2203b64
+# https://github.com/prajwalkr/SnapSudoku
